bc3bedf611d4e1c36aa30814c95a325774aaab20
Deivid <deivid652015@gmail.com>
2025-10-21 21:06:57 -0300

    feat: create Prismic client and routes configuration for Prismic API integration; add index file for slice components

---
+++ src/prismicio.ts
@@ -0,0 +1,32 @@
+import {
+  createClient as baseCreateClient,
+  type ClientConfig,
+  type Route,
+} from "@prismicio/client";
+import { enableAutoPreviews } from "@prismicio/next";
+import sm from "../slicemachine.config.json";
+
+export const repositoryName =
+  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || sm.repositoryName;
+
+
+const routes: Route[] = [
+
+
+
+];
+
+export const createClient = (config: ClientConfig = {}) => {
+  const client = baseCreateClient(repositoryName, {
+    routes,
+    fetchOptions:
+      process.env.NODE_ENV === "production"
+        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        : { next: { revalidate: 5 } },
+    ...config,
+  });
+
+  enableAutoPreviews({ client });
+
+  return client;
+};
